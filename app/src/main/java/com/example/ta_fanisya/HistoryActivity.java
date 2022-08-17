9b54780d48688febc2069bfacfe4b4343139cabb
bennypn <bennyperdana.jakarta@gmail.com>
2022-08-17 22:22:24 +0700

    fix regis & history sheet link

--- app/src/main/java/com/example/ta_fanisya/HistoryActivity.java
+++ app/src/main/java/com/example/ta_fanisya/HistoryActivity.java
@@ -38,8 +38,12 @@ public class HistoryActivity extends AppCompatActivity {
         btnSheet = findViewById(R.id.his_back_sheet);
 
         // To display the Recycler view linearly
-        recyclerView.setLayoutManager(
-                new LinearLayoutManager(this));
+//        recyclerView.setLayoutManager(
+//                new LinearLayoutManager(this));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
         // It is a class provide by the FirebaseUI to make a
         // query in the database to fetch appropriate data
@@ -51,10 +55,7 @@ public class HistoryActivity extends AppCompatActivity {
         // the Adapter class itself
         adapter = new historyAdapter(options);
         // Connecting Adapter class with the Recycler view*/
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setReverseLayout(true);
-        linearLayoutManager.setStackFromEnd(true);
-        recyclerView.setLayoutManager(linearLayoutManager);
+
         recyclerView.setAdapter(adapter);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
